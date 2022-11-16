c325433d1d8b0cad9618df1bdc2160c7c7c6e0c9
Kee Wook Lee <keewook2@gmail.com>
2022-11-16 09:03:11 +0900

    added selected related property and css for button

--- client/src/components/Button.tsx
+++ client/src/components/Button.tsx
@@ -1,27 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
-const ButtonWrapper = styled.button`
+type ButtonProps = {
+  text: string;
+};
+
+const ButtonWrapper = styled.button<{ selected: boolean }>`
   margin: 0;
   padding: 0.5rem 1rem;
 
+  width: 192px;
+  height: 40px;
+
   font-family: 'Montserrat', sans-serif;
-  font-size: 1rem;
+  font-size: 0.8rem;
   font-weight: 400;
   text-align: center;
   text-decoration: none;
 
   display: inline-block;
-  width: auto;
   border: none;
-  border-radius: 4px;
+  border-radius: 20px;
 
   box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
   cursor: pointer;
   transition: 0.5s;
 
-  background-color: #ccc;
-  color: #383838;
+  background-color: ${(props) => (props.selected ? '#425af5' : '#f4f4f4')};
+  color: ${(props) => (props.selected ? 'white' : '#383838')};
 
   &:active,
   &:hover,
@@ -31,10 +37,16 @@ const ButtonWrapper = styled.button`
   }
 `
 
-const Button: React.FC = () => {
+const Button: React.FC<ButtonProps> = ({ text }) => {
+  const [selected, setSelected] = useState(false);
+
+  const handleSelected = (e: React.MouseEvent<HTMLButtonElement>): void => {
+    e.preventDefault();
+    setSelected(prevSelect => !prevSelect);
+  }
   return (
-    <ButtonWrapper>
-      Text here
+    <ButtonWrapper onClick={handleSelected} selected={selected}>
+      {text}
     </ButtonWrapper>
   );
 }
