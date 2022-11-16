fc5c98afc20bd6f57dbe44ce3948e6efc22a3131
Kee Wook Lee <keewook2@gmail.com>
2022-11-16 09:02:49 +0900

    added default image for thumbnail

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,15 @@
-import React from 'react';
-import Button from './components/Button';
-import Textbox from './components/Textbox';
+import React, { useState } from 'react';
 import GlobalStyle from './GlobalStyle';
+import Content from './components/Content';
 
 const App: React.FC = () => {
+  const [imgURL, setImgURL] = useState('https://images.unsplash.com/photo-1620121478247-ec786b9be2fa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1489&q=80');
+
   return (
     <>
-      <GlobalStyle />
-      <div>Hello world</div>
-      <Button />
-      <Textbox />
-      <Textbox />
+      <GlobalStyle imgURL={imgURL}/>
+      <div>Thumbnail maker</div>
+      <Content imgURL={imgURL} />
     </>
   );
 };
--- client/src/GlobalStyle.tsx
+++ client/src/GlobalStyle.tsx
@@ -1,12 +1,29 @@
 import { createGlobalStyle } from "styled-components";
 
-const GlobalStyle = createGlobalStyle`
+interface Props {
+  imgURL: string;
+}
+
+const GlobalStyle = createGlobalStyle<Props>`
   body {
     font-family: 'Roboto Slab', 'Montserrat', sans-serif;
     line-height: 1.5;
     margin: 0;
     padding: 0;
     width: 100%;
+    background: url(${props => props.imgURL});
+
+    &:after {
+      position: fixed;
+      content: '';
+      top: 0;
+      left: 0;
+      display: block;
+      height: 100%;
+      width: 100%;
+      z-index: -100;
+      backdrop-filter: blur(10px);
+    }
   }
 `;
 
