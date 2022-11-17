4451b9192f0e4990b5f3fd49b5ba50b946c13730
Kee Wook Lee <keewook2@gmail.com>
2022-11-17 18:53:29 +0900

    added button selected state on reducer

--- client/src/reducer/index.ts
+++ client/src/reducer/index.ts
@@ -5,13 +5,35 @@ export type InputType = {
   category: string;
 };
 
+export type SelectedType = {
+  gradient: boolean;
+  solid: boolean;
+  image: boolean;
+  three: boolean;
+  two: boolean;
+  one: boolean;
+  textShadow: boolean;
+  colorContrast: boolean;
+  smallText: boolean;
+}
+
 export type StateType = {
   imgURL: string;
-  inputs: InputType
+  inputs: InputType;
+  selected: SelectedType;
 };
 
 export type ActionType = { type: 'CHANGE_IMAGE'; payload?: any; }
-  | { type: 'CHANGE_INPUT'; payload?: any; };
+  | { type: 'CHANGE_INPUT'; payload?: any; }
+  | { type: 'GRADIENT_CLICK'; }
+  | { type: 'SOLID_CLICK'; }
+  | { type: 'IMAGE_CLICK'; }
+  | { type: 'THREE_CLICK'; }
+  | { type: 'TWO_CLICK'; }
+  | { type: 'ONE_CLICK'; }
+  | { type: 'TEXTSHADOW_CLICK'; }
+  | { type: 'CONTRAST_CLICK'; }
+  | { type: 'SMALLTEXT_CLICK'; };
 
 export const initialState = {
   imgURL: 'https://images.unsplash.com/photo-1548318281-7da3085ce691?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1041&q=80',
@@ -19,6 +41,17 @@ export const initialState = {
     title: '>Enter title',
     subtitle: '>Enter subtitle',
     category: '>Enter category'
+  },
+  selected: {
+    gradient: false,
+    solid: false,
+    image: false,
+    three: true,
+    two: false,
+    one: false,
+    textShadow: false,
+    colorContrast: false,
+    smallText: false,
   }
 }
 
@@ -35,6 +68,90 @@ const reducer = (state: StateType, action: ActionType) => {
         inputs: {
           ...state.inputs,
           [action.payload.name]: action.payload.value
+        }
+      }
+    case 'GRADIENT_CLICK':
+      return {
+        ...state,
+        selected: {
+          ...state.selected,
+          gradient: true,
+          solid: false,
+          image: false,
+        }
+      }
+    case 'SOLID_CLICK':
+      return {
+        ...state,
+        selected: {
+          ...state.selected,
+          gradient: false,
+          solid: true,
+          image: false,
+        }
+      }
+    case 'IMAGE_CLICK':
+      return {
+        ...state,
+        selected: {
+          ...state.selected,
+          gradient: false,
+          solid: false,
+          image: true,
+        }
+      }
+    case 'THREE_CLICK':
+      return {
+        ...state,
+        selected: {
+          ...state.selected,
+          three: true,
+          two: false,
+          one: false,
+        }
+      }
+    case 'TWO_CLICK':
+      return {
+        ...state,
+        selected: {
+          ...state.selected,
+          three: false,
+          two: true,
+          one: false,
+        }
+      }
+    case 'ONE_CLICK':
+      return {
+        ...state,
+        selected: {
+          ...state.selected,
+          three: false,
+          two: false,
+          one: true,
+        }
+      }
+    case 'TEXTSHADOW_CLICK':
+      return {
+        ...state,
+        selected: {
+          ...state.selected,
+          textShadow: true,
+        }
+      }
+    case 'CONTRAST_CLICK':
+      return {
+        ...state,
+        selected: {
+          ...state.selected,
+          colorContrast: true,
+        }
+      }
+    case 'SMALLTEXT_CLICK':
+      return {
+        ...state,
+        selected: {
+          ...state.selected,
+          smallText: true,
         }
       }
     default:
